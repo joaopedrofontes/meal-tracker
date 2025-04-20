5451aad51f8617c299f36d82eb3d020973ff37d0
Joao Pedro Fontes <joaopedrofonttes@gmail.com>
2025-04-20 16:15:05 -0300

    update mealFood controller with dtos

--- src/main/java/com/fontes/mealtracker/controller/MealFoodController.java
+++ src/main/java/com/fontes/mealtracker/controller/MealFoodController.java
@@ -1,7 +1,12 @@
 package com.fontes.mealtracker.controller;
 
+import com.fontes.mealtracker.dto.mealFood.MealFoodMapper;
+import com.fontes.mealtracker.dto.mealFood.MealFoodRequestDTO;
+import com.fontes.mealtracker.dto.mealFood.MealFoodResponseDTO;
 import com.fontes.mealtracker.model.MealFood;
 import com.fontes.mealtracker.service.MealFoodService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -17,13 +22,17 @@ public class MealFoodController {
     }
 
     @PostMapping
-    public MealFood createMealFood(@RequestBody MealFood mealFood) {
-        return mealFoodService.save(mealFood);
+    public MealFoodResponseDTO createMealFood(@RequestBody @Valid MealFoodRequestDTO dto) {
+        MealFood mealFood = MealFoodMapper.toEntity(dto);
+        MealFood saved = mealFoodService.save(mealFood);
+        return MealFoodMapper.toMealFoodResponseDTO(saved);
     }
 
     @GetMapping(value = "/{id}")
-    public MealFood getMealFood(@PathVariable UUID id) {
-        return mealFoodService.findById(id).orElse(null);
+    public ResponseEntity<MealFoodResponseDTO> getMealFood(@PathVariable UUID id) {
+        return mealFoodService.findById(id)
+                .map(MealFoodMapper::toMealFoodResponseDTO)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
-
 }
