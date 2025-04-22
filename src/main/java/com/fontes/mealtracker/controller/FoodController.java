a6587a9b71bca7abc17739239f4517b9cbc9f0c4
Joao Pedro Fontes <joaopedrofonttes@gmail.com>
2025-04-22 10:24:51 -0300

    create food controller

---
+++ src/main/java/com/fontes/mealtracker/controller/FoodController.java
@@ -0,0 +1,33 @@
+package com.fontes.mealtracker.controller;
+
+import com.fontes.mealtracker.dto.food.FoodRequestDTO;
+import com.fontes.mealtracker.dto.food.FoodResponseDTO;
+import com.fontes.mealtracker.service.FoodService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api/food")
+public class FoodController {
+
+    private final FoodService foodService;
+
+    public FoodController(FoodService foodService) {
+        this.foodService = foodService;
+    }
+
+    @PostMapping
+    public ResponseEntity<FoodResponseDTO> createFood(@RequestBody  @Valid FoodRequestDTO dto) {
+        FoodResponseDTO response = foodService.save(dto);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping(value=  "/{id}")
+    public ResponseEntity<FoodResponseDTO> getFood(@PathVariable String id) {
+        return foodService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+}
