1c70d560220e553320d5c1198176203e5f7608d1
Joao Pedro Fontes <joaopedrofonttes@gmail.com>
2025-04-25 16:22:44 -0300

    create security configuration

---
+++ src/main/java/com/fontes/mealtracker/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.fontes.mealtracker.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/api/meal").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+}
